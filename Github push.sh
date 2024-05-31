47e8c2f66feec21712108c8dea04026354fcf3d3
Hello <160751203+chonvotri@users.noreply.github.com>
2024-05-31 22:16:13 +0700

    Update Github push.sh

--- Github push.sh
+++ Github push.sh
@@ -1,10 +1,10 @@
 echo "Đang chuẩn bị quá trình update file lên github ....."
-echo "cd /var/mobile/bonios.github.io"
+echo "cd /var/mobile/chonvotri.github.io/repo"
 echo "git add --all"
 echo "git commit -m "Init commit""
 echo "git push -u origin main"
-echo "bonios"
+echo "27th5"
 echo "ghp_g6DFVfAwsTtqFdYRSYpQUghm0u5Tc20JlC6"
 echo "xong!"
-echo " Make by BoniOs!"
+echo " Make by QUANGMINH!"
 exit 0
